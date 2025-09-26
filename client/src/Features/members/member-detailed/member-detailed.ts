86200b7146f23d22faab849947e7d9635e970e10
DESKTOP-7VVLG6T\DELL <Rina.pardeshi@nullplex.in>
2025-09-26 18:43:27 +0530

    partially done

--- client/src/Features/members/member-detailed/member-detailed.ts
+++ client/src/Features/members/member-detailed/member-detailed.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../Core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../Types/members';
 import { filter, Observable } from 'rxjs';
 import { AgePipe } from '../../../Core/pipes/age-pipe';
+import { AccountService } from '../../../Core/services/account-service';
 
 
 @Component({
@@ -16,9 +17,14 @@ import { AgePipe } from '../../../Core/pipes/age-pipe';
 export class MemberDetailed implements OnInit {
 
   private route = inject(ActivatedRoute);
+  private accountService=inject(AccountService);
+  protected memberService=inject(MemberService);
   private router = inject(Router);
   protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('profile');
+  protected isCurrentUser=computed(()=>{
+return this.accountService.currentUser()?.id===this.route.snapshot.paramMap.get('id');
+  })
 
   ngOnInit(): void {
     this.route.data.subscribe({
--- client/src/Features/members/member-profile/member-profile.ts
+++ client/src/Features/members/member-profile/member-profile.ts
@@ -1,22 +1,55 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, ViewChild, viewChild } from '@angular/core';
 import { AccountService } from '../../../Core/services/account-service';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../Types/members';
+import { EditableMember, Member } from '../../../Types/members';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../Core/services/member-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../Core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit {
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm;
   private route = inject(ActivatedRoute);
+  protected memberServices = inject(MemberService);
+  private toast = inject(ToastService);
   protected member = signal<Member | undefined>(undefined);
+  protected editableMember: EditableMember={
+    displayName:'',
+description:'',
+city:'',
+country:''
+  };
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data['member']);
     })
+    this.editableMember = {
+      displayName: this.member()?.displayName || '',
+      description: this.member()?.description || '',
+      city: this.member()?.city || '',
+      country: this.member()?.country || '',
+    }
   }
+
+  updateProfile() {
+    if (!this.member()) return;
+    const updateMember = { ...this.member(), ...this.editableMember }
+    console.log(updateMember);
+    this.toast.success('Profile updated sucessfully');
+    this.memberServices.editMode.set(false);
+  }
+
+  ngOnDestroy(): void {
+    if (this.memberServices.editMode()) {
+      this.memberServices.editMode.set(false);
+    }
+  }
+
 }
